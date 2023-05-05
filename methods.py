9d2f3e43642d863f2b9d9837f4024f700adfdad9
IAGO DE OLIVEIRA AURICHIO <21.00236-3@maua.br>
2023-05-05 09:04:10 -0300

    Ex1 aula 9 4 opreações da calculadora
    Signed-off-by: IAGO DE OLIVEIRA AURICHIO <21.00236-3@maua.br>

--- methods.py
+++ methods.py
@@ -5,3 +5,20 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+
+def soma(num1, num2):
+    soma = num1 + num2
+    return soma
+
+def subtracao(num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+def multiplicacao(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+
+def divisao(num1, num2):
+    divisao = num1 / num2
+    return divisao
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,48 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+
+def test_soma():
+    # given a num1 of 10 and a num2 of 5
+    num1 = 10
+    num2 = 5
+
+    # when we calculate the soma
+    output = methods.soma(num1, num2)
+
+    # then the area should be 15
+    assert output == 15
+
+def test_subtracao():
+    # given a num1 of 20 and a num2 of 10
+    num1 = 20
+    num2 = 10
+
+    # when we calculate the subtracao
+    output = methods.subtracao(num1, num2)
+
+    # then the area should be 10
+    assert output == 10
+
+def test_multiplicacao():
+    # given a num1 of 5 and a num2 of 10
+    num1 = 5
+    num2 = 10
+
+    # when we calculate the multiplicacao
+    output = methods.multiplicacao(num1, num2)
+
+    # then the area should be 50
+    assert output == 50
+
+def test_divisao():
+    # given a num1 of 10 and a num2 of 2
+    num1 = 10
+    num2 = 2
+
+    # when we calculate the divisao
+    output = methods.divisao(num1, num2)
+
+    # then the area should be 5
+    assert output == 5
